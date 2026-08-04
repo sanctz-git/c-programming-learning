968435973c1e673c28e47bdeab28d698f1c8b95e
sanctz-git <sanctz07317@gmail.com>
2026-08-04 13:10:21 +0800

    翁恺堂上switch习题

---
+++ chengji.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int grade;
+    scanf("%d", &grade);
+    int a = grade / 10;
+
+    switch (a)
+    {
+    case 10:
+        printf("A\n");
+        break;
+    case 9:
+        printf("A\n");
+        break;
+    case 8:
+        printf("B\n");
+        break;
+    case 7:
+        printf("C\n");
+        break;
+    case 6:
+        printf("D\n");
+        break;
+    default:
+        printf("E\n");
+        break;
+    }
+    return 0;
+}
