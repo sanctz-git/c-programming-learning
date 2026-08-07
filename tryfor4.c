cc6b28730366ede5a744a8dbd77ac948bd14fd83
sanctz-git <sanctz07317@gmail.com>
2026-08-07 13:24:12 +0800

    浙大C第四版11题（for）

---
+++ tryfor4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int N;
+    double sum = 0;
+    scanf("%d", &N);
+
+    for (int i = 1; i <= N; i++)
+    {   sum += 1.0 / (2*i - 1); 
+        /* code */
+    }
+    
+    printf("sum = %.6f\n", sum);
+
+    return 0;
+}
