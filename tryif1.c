9179d36b97d04c984a8708275dcb573800f67c1a
sanctz-git <sanctz07317@gmail.com>
2026-09-17 13:31:46 +0800

    练习3-7 成绩转换

---
+++ tryif1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int score;
+    scanf("%d", &score);
+
+    if (score >= 90)
+    {   printf("A\n");
+        /* code */
+    }else if (score >= 80)
+    {   printf("B\n");
+        /* code */
+    }else if (score >= 80)
+    {   printf("B\n");
+        /* code */
+    }else if (score >= 70)
+    {   printf("C\n");
+        /* code */
+    }else if (score >= 60)
+    {   printf("D\n");
+        /* code */
+    }else
+    {   printf("E\n");
+        /* code */
+    }
+    return 0;
+    
+}
